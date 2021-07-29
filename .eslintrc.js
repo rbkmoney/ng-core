4303d91e96091e88fbd312d851475cbf7997b626
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-07-29 18:54:01 +0300

    FR-623: Angular 12 (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,65 @@
+const rules = require('@rbkmoney/eslint-plugin/lib/rules');
+
+const baseTsRules = {
+    ...rules.createImportOrderRule({
+        internalPathsPattern: '@cc/**',
+    }),
+    // TODO: pretenders for error
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-misused-promises': 'warn',
+    '@typescript-eslint/unbound-method': 'warn',
+    '@typescript-eslint/restrict-plus-operands': 'warn',
+    '@typescript-eslint/restrict-template-expressions': 'warn',
+};
+
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: ['plugin:prettier/recommended', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    root: true,
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'sort-imports': [
-            'error',
-            {
-                ignoreCase: false,
-                ignoreDeclarationSort: false,
-                ignoreMemberSort: false,
-                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-                allowSeparatedGroups: false,
+    overrides: [
+        {
+            files: ['*.ts'],
+            parserOptions: {
+                project: ['tsconfig.json', 'e2e/tsconfig.json'],
+                createDefaultProgram: true,
             },
-        ],
-    },
+            extends: [
+                'plugin:@rbkmoney/typescript',
+                'plugin:@rbkmoney/angular',
+                'plugin:@rbkmoney/lodash',
+                'plugin:@rbkmoney/prettier',
+            ],
+            rules: {
+                ...baseTsRules,
+                ...rules.createAngularSelectorRules({ prefix: 'cc' }),
+                // TODO: pretenders for error
+                '@typescript-eslint/no-floating-promises': 'warn',
+            },
+        },
+        {
+            files: ['*.spec.ts'],
+            parserOptions: {
+                project: ['tsconfig.json', 'e2e/tsconfig.json'],
+                createDefaultProgram: true,
+            },
+            extends: [
+                'plugin:@rbkmoney/typescript',
+                'plugin:@rbkmoney/angular',
+                'plugin:@rbkmoney/jasmine',
+                'plugin:@rbkmoney/lodash',
+                'plugin:@rbkmoney/prettier',
+            ],
+            rules: baseTsRules,
+        },
+        {
+            files: ['*.html'],
+            extends: ['plugin:@angular-eslint/template/recommended'],
+            rules: {
+                // TODO: pretenders for error
+                '@angular-eslint/template/no-negated-async': 'warn',
+            },
+        },
+    ],
 };
