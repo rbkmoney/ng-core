618efc35a0bfe6fc311eb92d09638fb8570b82bc
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-09-15 18:57:16 +0300

    fix: upd typings

--- projects/sentry/src/lib/create-sentry-providers.ts
+++ projects/sentry/src/lib/create-sentry-providers.ts
@@ -1,10 +1,9 @@
 import { ErrorHandler, Provider } from '@angular/core';
-import { Router } from '@angular/router';
 import { ErrorHandlerOptions, TraceService } from '@sentry/angular';
 
 import { ErrorHandler as CustomErrorHandler } from './error-handler';
 
-export function createSentryProviders(deps: [typeof Router], options?: ErrorHandlerOptions): Provider[] {
+export function createSentryProviders(deps: unknown[], options?: ErrorHandlerOptions): Provider[] {
     return [
         {
             provide: ErrorHandler,
